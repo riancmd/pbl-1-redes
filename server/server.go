69600e90c840e7b04473d52813f36753549d9ae6
riancmd <rian.dssantos@gmail.com>
2025-09-06 14:08:20 -0300

    ✨ feat: criou listener do server

--- server/server.go
+++ server/server.go
@@ -1,1 +1,26 @@
 package server
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+	address := ":8080"          //porta usada
+	envVar := os.Getenv("PORT") // usa env para pode trocar a porta qndo preciso
+
+	if envVar != "" { // coloca porta definida como porta
+		address = envVar
+	}
+
+	listener, error := net.Listen("tcp", address)
+
+	// verifica erro na conexão
+	if error != nil {
+		panic(error) // para a execução e sinaliza erro
+	}
+
+	fmt.Println("Servidor do Alucinari ouvindo na porta", address)
+
+}
